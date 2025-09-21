51e11155e227a0ed508a108a76bc61d8af7d9b43
JonahSagers <24sagjon@hawken.edu>
2025-09-21 16:27:19 -0400

    Add note sprites and animation

--- Assets/Scripts/NoteMove.cs
+++ Assets/Scripts/NoteMove.cs
@@ -16,6 +16,12 @@ public class NoteMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float substep = Song.elapsed - (int)Song.elapsed;
+        float substepScale = 1.2f - substep/5;
+        transform.localScale = new Vector3(substepScale, substepScale, 1);
+        float subSine = Mathf.Sin(Song.elapsed * Mathf.PI);
+        transform.rotation = Quaternion.Euler(0, 0, subSine * Mathf.Abs(subSine) * 10);
+
         travelTime += (Time.realtimeSinceStartup - lastTime) * (Song.bpm / 60);
         lastTime = Time.realtimeSinceStartup;
         transform.position = Vector3.Lerp(startPos, target.position, (travelTime - Song.timeSig + Song.timeSig / Song.scrollSpeed) / (Song.timeSig / Song.scrollSpeed));
