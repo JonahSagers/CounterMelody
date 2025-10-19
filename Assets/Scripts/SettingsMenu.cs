a1587174fca7600248a6452214156a7bf65981e6
JonahSagers <jonah.sagers@gmail.com>
2025-10-19 16:34:56 -0400

    Enter and exit settings menu

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,20 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class SettingsMenu : MonoBehaviour
 {
     public Slider scrollSpeed;
     public Toggle measureLine;
     public Toggle metronome;
+    private PlayerControls playerControls;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerControls = new PlayerControls();
+        playerControls.UI.Enable();
+        playerControls.UI.Cancel.performed += ExitMenu;
+    }
+
+    private void OnDestroy()
+    {
+        playerControls.UI.Cancel.performed -= ExitMenu;
+    }
+
+    private void ExitMenu(InputAction.CallbackContext context)
+    {
+        ExitMenu(); // just call the parameterless version
+    }
+
+    public void ExitMenu()
+    {
+        Debug.Log("Closing Menu");
+        playerControls.Player.Disable();
+        transform.parent.gameObject.SetActive(false);
     }
 
     public void RefreshSettings()
     {
-        Debug.Log("Updating Settings" + (scrollSpeed.value / 100));
         PlayerData.scrollSpeed = scrollSpeed.value / 100;
         PlayerData.measureLine = measureLine.isOn;
         PlayerData.metronome = metronome.isOn;
